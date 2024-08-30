05b94cd9df3bd7a562e40184b59844094c197b15
Brimo Battaro <125156578+battaro@users.noreply.github.com>
2024-08-30 20:18:52 +0300

    initial commit

---
+++ HelloWorld.c
@@ -0,0 +1,8 @@
+#include <stdio.h> //when we say "std" we are saying standard, "io" stand for input and output
+
+int main()
+{
+    printf("Hello World\n");
+    printf("I like 15 yo kids");
+    return 0;
+}
